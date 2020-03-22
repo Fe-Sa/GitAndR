91aa15d6eb370da98513fb4d075f9b780a3387fb
Felipe Sanchez <fsg2612@gmail.com>
2020-03-22 17:55:23 -0400

    Added more info on pushing and pulling changes to and from GitHub.

--- test.R
+++ test.R
@@ -4,14 +4,14 @@ get-yesterday <- function(){
  #Adding a commment here to commit within GitHub
 }
 
-#Section 1. Work Flow :
+#Section 1. Basic Work Flow :
   # 1. Edit code
   # 2. Save the script
   # 3. Got to the commit tab in the env pane
   # 4. Select the script and select Commit 
   # 5. Add a commit message (it wont let you commit without a message)
-  # 6. Select commit
-  # 7. Push to repositiory in GitHub( See section 3)
+  # 6. Select the commit button to Commit changes
+ 
 
 #Section 2. Reverting to older versions of the script in R 
 ## *You need to get the Git id (SHA)*
@@ -30,12 +30,16 @@ get-yesterday <- function(){
   #   you will need to create a personal access token.(useful link)
   #3. Select the repo permission box and give it a note
   #4. *Copy the token. You only get one shot at copying the token.Once you close the window
-  #   you will loose that token. The best way to store is in the r env file
+  #   you will lose that token. The best way to store is in the r env file
   #5. Back in r, use the edit_r_environ() function
   #6. Add the token as follows: GITHUB_PAT = 'Copied token'
   #7. Restart the R session. Session -> Restart R session
   #8. Re-load the usethis package
   #9. use_github(protocol = 'https', auth_token = Sys.getenv("GITHUB_PAT")) -> RUN
   #10.Now the repo has been created with the title of the project. 
+  #11. Once the repo has been created, follow the steps in section 1, with the 
+  #    added step of "Pushing" the commit to the GitHub repo. 
+  #12. You can also make changes within GitHub using the edit button and then "Pull" 
+  #    back those changes into the R script. 
 
   
